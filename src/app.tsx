8adb23e676482095307992c412e2469b1e3159ff
Brandon <brand.t2004@icloud.com>
2026-04-13 10:48:39 +0100

    routing for sidebar sections

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,19 @@
-import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider} from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Team from "./scenes/Team";
+import Contacts from "./scenes/Contacts";
+import Invoices from "./scenes/Invoices";
+import Form from "./scenes/Form";
+import FAQ from "./scenes/FAQ";
+import Bar from "./scenes/Bar";
+import Pie from "./scenes/Pie";
+import Line from "./scenes/Line";
+import Geography from "./scenes/Geography";
+
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -10,8 +23,22 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
